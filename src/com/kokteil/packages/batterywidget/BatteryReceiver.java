b7b040e3ce9bdb43a574240027db15f90181ee6d
Erkan Molla <molla.erkan@gmail.com>
2012-04-09 14:14:12 +0200

    text visual update

--- src/com/kokteil/packages/batterywidget/BatteryReceiver.java
+++ src/com/kokteil/packages/batterywidget/BatteryReceiver.java
@@ -24,28 +24,28 @@ import android.os.BatteryManager;
 
 public class BatteryReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		
-		if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)){
-			try {
-				Preferences batteryInfo  =  new Preferences(Constants.BATTERY_INFO, context);
-				
-				batteryInfo.setValue(Constants.STATUS, intent.getIntExtra(Constants.STATUS, 
-																BatteryManager.BATTERY_STATUS_UNKNOWN));
-				batteryInfo.setValue(Constants.PLUG, intent.getIntExtra(Constants.PLUG, 0));
-				batteryInfo.setValue(Constants.LEVEL, intent.getIntExtra(Constants.LEVEL, 0));
-				batteryInfo.setValue(Constants.SCALE, intent.getIntExtra(Constants.SCALE, 0));
-				batteryInfo.setValue(Constants.VOLTAGE, intent.getIntExtra(Constants.VOLTAGE, 0));
-				batteryInfo.setValue(Constants.TEMPERATURE, intent.getIntExtra(Constants.TEMPERATURE, 0));
-				batteryInfo.setValue(Constants.TECHNOLOGY, intent.getStringExtra(Constants.TECHNOLOGY));
-				batteryInfo.setValue(Constants.HEALTH, intent.getIntExtra(Constants.HEALTH, 
-																BatteryManager.BATTERY_HEALTH_UNKNOWN));
-				
-				context.startService(new Intent(context, BatteryUpdateService.class));
-	            
-			} catch (Exception e) {}
-		}
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)){
+            try {
+                Preferences batteryInfo  =  new Preferences(Constants.BATTERY_INFO, context);
+
+                batteryInfo.setValue(Constants.STATUS, intent.getIntExtra(Constants.STATUS, 
+                                                      BatteryManager.BATTERY_STATUS_UNKNOWN));
+                batteryInfo.setValue(Constants.PLUG, intent.getIntExtra(Constants.PLUG, 0));
+                batteryInfo.setValue(Constants.LEVEL, intent.getIntExtra(Constants.LEVEL, 0));
+                batteryInfo.setValue(Constants.SCALE, intent.getIntExtra(Constants.SCALE, 0));
+                batteryInfo.setValue(Constants.VOLTAGE, intent.getIntExtra(Constants.VOLTAGE, 0));
+                batteryInfo.setValue(Constants.TEMPERATURE, intent.getIntExtra(Constants.TEMPERATURE, 0));
+                batteryInfo.setValue(Constants.TECHNOLOGY, intent.getStringExtra(Constants.TECHNOLOGY));
+                batteryInfo.setValue(Constants.HEALTH, intent.getIntExtra(Constants.HEALTH, 
+                                                      BatteryManager.BATTERY_HEALTH_UNKNOWN));
+
+                context.startService(new Intent(context, BatteryUpdateService.class));
+
+            } catch (Exception e) {}
+        }
+    }
 
 }
--- src/com/kokteil/packages/batterywidget/OnWidgetClick.java
+++ src/com/kokteil/packages/batterywidget/OnWidgetClick.java
@@ -25,124 +25,125 @@ import android.widget.TextView;
 
 public class OnWidgetClick extends Activity{
 	
-	private TextView 	mStatusView;
-	private TextView	mPlugView;
-	private TextView 	mLevelView;
-	private TextView 	mScaleView;
-	private TextView	mVoltageView;
-	private TextView	mTemperatureView;
-	private TextView	mTechnologyView;
-	private TextView	mHealthView;
-	
-	private Preferences 	mBatteryInfo;
-	
-	@Override
-	protected void onCreate(Bundle bundle){
-		super.onCreate(bundle);
-		
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
-                			WindowManager.LayoutParams.FLAG_BLUR_BEHIND);  
-		
-		setContentView(R.layout.battery_info_view);
+    private TextView 	mStatusView;
+    private TextView	mPlugView;
+    private TextView 	mLevelView;
+    private TextView 	mScaleView;
+    private TextView	mVoltageView;
+    private TextView	mTemperatureView;
+    private TextView	mTechnologyView;
+    private TextView	mHealthView;
 
-		mStatusView      =  (TextView) findViewById(R.id.state);
-		mPlugView        =  (TextView) findViewById(R.id.plug);
-		mLevelView       =  (TextView) findViewById(R.id.level);
-		mScaleView       =  (TextView) findViewById(R.id.scale);
-		mVoltageView     =  (TextView) findViewById(R.id.voltage);
-		mTemperatureView =  (TextView) findViewById(R.id.temperature);
-		mTechnologyView  =  (TextView) findViewById(R.id.technology);
-		mHealthView      =  (TextView) findViewById(R.id.health);
-		
-		updateBatteryInfoView();		
-		
-	}
-	
-	public void updateBatteryInfoView(){
-		
-		try {
-			
-			mBatteryInfo = new Preferences(Constants.BATTERY_INFO, getApplicationContext());
-			
-	        mStatusView.setText(getBatteryStatus());
-	        mPlugView.setText(getBatteryPlug());
-			mLevelView.setText(mBatteryInfo.getValue(Constants.LEVEL, 0)+
-                                this.getString(R.string.batteryLevelSymbol));
-			mScaleView.setText(mBatteryInfo.getValue(Constants.SCALE, 0)+
-                                this.getString(R.string.Empty));
-			mVoltageView.setText(mBatteryInfo.getValue(Constants.VOLTAGE, 0)+
-                                this.getString(R.string.batteryVoltSymbol));
-			mTemperatureView.setText(getBatteryTemperature()+
-                                this.getString(R.string.batteryCelsiusSymbol));
-			mTechnologyView.setText(mBatteryInfo.getValue(Constants.TECHNOLOGY,"")+
-                                this.getString(R.string.Empty));
-			mHealthView.setText(getBatteryHealth());
-			
-		}catch (Exception e) {}
-		
-	}
-	
-	
-	private int getBatteryStatus(){
-		
-		int status = mBatteryInfo.getValue(Constants.STATUS, 0);
-		switch (status){
-		
-        case BatteryManager.BATTERY_STATUS_UNKNOWN:
-            return R.string.Unknown;
-        case BatteryManager.BATTERY_STATUS_CHARGING:
-            return R.string.batteryStatusCharging;
-        case BatteryManager.BATTERY_STATUS_DISCHARGING:
-            return R.string.batteryStatusDischarging;
-        case BatteryManager.BATTERY_STATUS_FULL:
-            return R.string.batteryStatusFull;
-        case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
-            return R.string.batteryStatusNotCharging;
-        default:
-            return R.string.Unknown;
-		}
-	}
+    private Preferences 	mBatteryInfo;
 
-	
-	private int getBatteryPlug(){
-		
-		int plug = mBatteryInfo.getValue(Constants.PLUG, 0);
-		switch (plug) {
-		
-		case BatteryManager.BATTERY_PLUGGED_AC:
-			return R.string.batteryPlugAC;
-		case BatteryManager.BATTERY_PLUGGED_USB:
-			return R.string.batteryPlugUSB;
-		default:
-			return R.string.batteryPlugNone;
-		}
-	}
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
 
-	private int getBatteryHealth(){
-		
-		int health = mBatteryInfo.getValue(Constants.HEALTH, 0);
-		switch (health){
-		
-		case BatteryManager.BATTERY_HEALTH_DEAD:
-			return R.string.batteryHealthDead;
-		case BatteryManager.BATTERY_HEALTH_GOOD:
-			return R.string.batteryHealthGood;
-		case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
-			return R.string.batteryHealthOverVoltage;
-		case BatteryManager.BATTERY_HEALTH_OVERHEAT:
-			return R.string.batteryHealthOverheat;
-		case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
-			return R.string.batteryHealthUnspecifiedFailure;
-		default:
-			return R.string.Unknown;
-		}
-	}
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
+                             WindowManager.LayoutParams.FLAG_BLUR_BEHIND);  
+
+        setContentView(R.layout.battery_info_view);
+
+        mStatusView      =  (TextView) findViewById(R.id.state);
+        mPlugView        =  (TextView) findViewById(R.id.plug);
+        mLevelView       =  (TextView) findViewById(R.id.level);
+        mScaleView       =  (TextView) findViewById(R.id.scale);
+        mVoltageView     =  (TextView) findViewById(R.id.voltage);
+        mTemperatureView =  (TextView) findViewById(R.id.temperature);
+        mTechnologyView  =  (TextView) findViewById(R.id.technology);
+        mHealthView      =  (TextView) findViewById(R.id.health);
+
+        updateBatteryInfoView();		
+
+    }
+
+    public void updateBatteryInfoView(){
+    
+        try {
+
+            mBatteryInfo = new Preferences(Constants.BATTERY_INFO, getApplicationContext());
+
+            mStatusView.setText(getBatteryStatus());
+            mPlugView.setText(getBatteryPlug());
+            mLevelView.setText(mBatteryInfo.getValue(Constants.LEVEL, 0)+
+                                        this.getString(R.string.batteryLevelSymbol));
+            mScaleView.setText(mBatteryInfo.getValue(Constants.SCALE, 0)+
+                                        this.getString(R.string.Empty));
+            mVoltageView.setText(mBatteryInfo.getValue(Constants.VOLTAGE, 0)+
+                                        this.getString(R.string.batteryVoltSymbol));
+            mTemperatureView.setText(getBatteryTemperature()+
+                                        this.getString(R.string.batteryCelsiusSymbol));
+            mTechnologyView.setText(mBatteryInfo.getValue(Constants.TECHNOLOGY,"")+
+                                        this.getString(R.string.Empty));
+            mHealthView.setText(getBatteryHealth());
+
+        }catch (Exception e) {}
+
+    }
+
+
+    private int getBatteryStatus(){
+
+        int status = mBatteryInfo.getValue(Constants.STATUS, 0);
+        switch (status){
+
+            case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                return R.string.Unknown;
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                return R.string.batteryStatusCharging;
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                return R.string.batteryStatusDischarging;
+            case BatteryManager.BATTERY_STATUS_FULL:
+                return R.string.batteryStatusFull;
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                return R.string.batteryStatusNotCharging;
+            default:
+                return R.string.Unknown;
+        }
+    }
 
 	
-	private int getBatteryTemperature(){
-		int temperature = mBatteryInfo.getValue(Constants.TEMPERATURE, 0);
-		temperature = temperature / 10;
-		return temperature;
-	}
+    private int getBatteryPlug(){
+
+        int plug = mBatteryInfo.getValue(Constants.PLUG, 0);
+        switch (plug) {
+
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                return R.string.batteryPlugAC;
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                return R.string.batteryPlugUSB;
+            default:
+                return R.string.batteryPlugNone;
+        }
+    }
+
+    private int getBatteryHealth(){
+
+        int health = mBatteryInfo.getValue(Constants.HEALTH, 0);
+        switch (health){
+
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                return R.string.batteryHealthDead;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                return R.string.batteryHealthGood;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                return R.string.batteryHealthOverVoltage;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                return R.string.batteryHealthOverheat;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                return R.string.batteryHealthUnspecifiedFailure;
+            default:
+                return R.string.Unknown;
+        }
+    }
+
+	
+    private int getBatteryTemperature(){
+        int temperature = mBatteryInfo.getValue(Constants.TEMPERATURE, 0);
+        temperature = temperature / 10;
+        return temperature;
+    }
 
 }
+
